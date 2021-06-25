9e30ca0fb8bb541ea7470e9adfc5aeebb9849075
PhillyJB <philipbuuza@gmail.com>
2021-06-25 17:44:56 +0000

    addeda base.html file

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#routes and views for the html templates.
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/glossary")
+def glossary():
+    return render_template("glossary.html")
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=os.environ.get("PORT", "5000"), 
+        debug=True)
