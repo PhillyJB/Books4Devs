e46be244c836401431849885471e1092dbc4ee98
PhillyJB <philipbuuza@gmail.com>
2021-06-27 23:19:45 +0000

    added requirements.txt and Procfile

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, render_template
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
@@ -17,6 +19,6 @@ def glossary():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=True)
