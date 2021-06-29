90c2ba38342077ee0aab6c2d5692e40d59ce125c
PhillyJB <philipbuuza@gmail.com>
2021-06-29 11:31:09 +0000

    added login functionality

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def register():
         # on our form to check if the key value matches #anything in our db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
+
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
@@ -54,6 +55,32 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #we will check to see if the username already exists in our db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            #we need to make sure the existing hash password matches the input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                #if the password is invalid/does not match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        
+        else:
+            # username does not exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+            
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
