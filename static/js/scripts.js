85ded60b865871d6917476f62574ba75f5bae176
PhillyJB <philipbuuza@gmail.com>
2021-07-03 11:28:59 +0000

    changed to a materialize layout from bootstrap

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+//Side nave-bar for mobile initialisation
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+//copyright span year date
+$("#copyright-date").text(new Date().getFullYear());
