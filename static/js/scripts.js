26c42766fdf0ab3b8805c573d26ce90f97bf0685
PhillyJB <philipbuuza@gmail.com>
2021-07-04 13:24:49 +0000

    ammended and added defensive deletion programming

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,12 +2,21 @@
 $(document).ready(function(){
   //Side nave-bar for mobile initialisation
     $('.sidenav').sidenav();
-//Terms and conditions terms and cons
+
+//Terms and conditions hide then toggle
     $('#termsCon').hide();
-    
     $('#termsConTrigger').click(function(){
-      $('#termsCon').toggle();
+      $('#termsCon').toggle(300);
     });
+
+    //J query defence programming for are you sure 
+    $('.deletion-alert').click(function(){
+      if( confirm("are you sure you want to delete this book? Click OK to Confirm deletion or Cancel to go back")){
+      return true;
+      } else{
+      return false;
+      }
+      });
   });
 
 //copyright span year date
