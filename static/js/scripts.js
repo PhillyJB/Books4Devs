6ec42e58decaa2ca9092c100178d03cb53c70a47
PhillyJB <philipbuuza@gmail.com>
2021-07-04 10:51:50 +0000

    ammended add_book criteria

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,13 @@
 //Side nave-bar for mobile initialisation
 $(document).ready(function(){
+  //Side nave-bar for mobile initialisation
     $('.sidenav').sidenav();
+//Terms and conditions terms and cons
+    $('#termsCon').hide();
+    
+    $('#termsConTrigger').click(function(){
+      $('#termsCon').toggle();
+    });
   });
 
 //copyright span year date
